34452435d443e036a9c2e2ec69f647b78c11e28c
quinns25 <24011941@myrp.edu.sg>
2026-01-07 15:09:16 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+// include the required packages
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+const port = 3000;
+
+// database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit:0,
+};
+
+//intialize express app
+const app = express();
+//helps app to read JSON
+app.use(express.json());
+
+// start the server
+app.listen(port, () => {
+    console.log('Server running on port', port);
+});
+
+// example route: get all cards
+app.get('/allcards', async(req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message:'Server error for allcards'});
+    }
+});
