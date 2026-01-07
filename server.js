81ea2ef7a676550cdc3d8db99387e8167e5e233b
quinns25 <24011941@myrp.edu.sg>
2026-01-07 15:33:37 +0800

    Change MySQL import to use promise-based version

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // include the required packages
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 const port = 3000;
 
